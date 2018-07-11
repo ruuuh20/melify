c1569c9890b1150205914476ccafba4a6d940123
ruuuh20 <pkim2320@gmail.com>
2018-07-11 11:21:04 -0400

    rspotify and authenticate

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,15 @@
 class ArtistsController < ApplicationController
 
   def index
-  end
+    RSpotify.authenticate(ENV['CLIENT_ID'], ENV['CLIENT_SECRET'])
+    #authenticate before each request?
+
+   if !params[:artist_name].empty?
+     @artists = RSpotify::Artist.search(params[:artist_name])
+   else
+     redirect_to root_path
+   end
+ end
 
   def show
   end
